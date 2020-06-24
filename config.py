c8e42ae8a182a1320c344a67be7bb466f9fa1bdc
Rohan Kadkol <rohankd100@gmail.com>
2020-06-23 20:08:34 -0500

    Updated the database uri in config

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 # TODO: Modify it to match your database
-SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/{}'.format(
-    'postgres', 'password', 'localhost:5432', 'capstone_db')
+# SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/{}'.format(
+#     'postgres', 'password', 'localhost:5432', 'capstone_db')
+
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+if not SQLALCHEMY_DATABASE_URI:
+    SQLALCHEMY_DATABASE_URI = 'postgres://{}:{}@{}/{}'.format(
+        'postgres', 'password', 'localhost:5432', 'capstone_db')
+
 
 def setup_db(app, database_path=SQLALCHEMY_DATABASE_URI):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path
